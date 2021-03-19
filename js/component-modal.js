8e706f62cc85af96ac455ee40c93405c6c5f01db
Hilario <hmaldonado0@ucol.mx>
2021-03-19 02:03:07 -0600

    Muchas cosas
    Nuevos componentes: modal(WIP), con clase en js.
    Actualizados los componentes: navbar, dropdown, pagination, cars-cards y foote.
    Ahora se hace uso de grid en ciertas partes para un mejor comportamiento.

---
+++ js/component-modal.js
@@ -0,0 +1,26 @@
+const Modal = function (select) {
+    let selector = select;
+    let element = document.querySelector(selector);
+    let state = false;
+    let show = function () {
+        element.classList.remove("hidden");
+        state = true;
+    }
+    let hide = function () {
+        element.classList.add("hidden");
+        state = false;
+    }
+    let closeButton = element.querySelector("div > .modal-header > i");
+    closeButton.addEventListener("click", e => {
+        hide();
+    });
+    element.addEventListener("click", e => {
+        if (e.target.classList.contains("modal")) {
+            hide();
+        }
+    });
+    return {
+        show : show,
+        hide : hide
+    };
+}
