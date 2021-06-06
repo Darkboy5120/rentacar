fad29dcc671c6877533fbbb01d189a3fec4f3a70
Hilario <hmaldonado0@ucol.mx>
2021-06-06 18:14:55 -0500

    Correccion eb apis

--- model/apis/mobile/get_requested_cars.php
+++ model/apis/mobile/get_requested_cars.php
@@ -9,6 +9,7 @@ if (!isset($_REQUEST["user_id"])
 }
 
 $user_id = $_REQUEST["user_id"];
+$user_id_sql = null;
 $fase = $_REQUEST["fase"];
 $fase_sql = NULL;
 switch ($fase) {
@@ -27,6 +28,22 @@ $mi0->begin();
 
 $mi0->query("
     SELECT
+        tipo
+    FROM
+        usuario
+    WHERE pk_usuario = ?",
+    $user_id
+);
+if ($mi0->result->num_rows != 1) {
+    $mi0->end("rollback", -2, NULL);
+}
+
+$user_type = $mi0->result->fetch_all(MYSQLI_ASSOC)[0]["tipo"];
+$user_id_sql = ($user_type == "1") ? "renta.fk_arrendatario"
+    : "renta.fk_conductor";
+
+$mi0->query("
+    SELECT
         auto.pk_auto,
         auto.fk_administrador,
         auto_imagen.imagen_ruta,
@@ -37,7 +54,14 @@ $mi0->query("
         renta.costo,
         renta.fase,
         renta.fechahora_entrega,
-        renta.fechahora_devolucion
+        renta.fechahora_devolucion,
+        (
+            SELECT
+                pk_reporte_devolucion
+            FROM
+                reporte_devolucion
+            WHERE fk_renta = renta.pk_renta
+        ) as tiene_reporte
     FROM
         auto
     LEFT JOIN
@@ -47,7 +71,7 @@ $mi0->query("
             AND auto_modelo.pk_auto_modelo = auto.fk_auto_modelo
             AND auto_modelo.fk_auto_marca = auto_marca.pk_auto_marca
             AND auto.pk_auto = renta.fk_auto)
-    WHERE renta.fk_arrendatario = ? AND renta.fase IN $fase_sql",
+    WHERE $user_id_sql = ? AND renta.fase IN $fase_sql",
     $user_id
 );
 if ($mi0->result->num_rows > 0) {
@@ -56,5 +80,5 @@ if ($mi0->result->num_rows > 0) {
     );
     $mi0->end("commit", 0, $data);
 } else {
-    $mi0->end("rollback", -2, NULL);
+    $mi0->end("rollback", -3, NULL);
 }
--- model/apis/mobile/update_rent_phase.php
+++ model/apis/mobile/update_rent_phase.php
@@ -3,31 +3,50 @@ require "../users/root.php";
 require "../utils/user_validation.php";
 
 if (!isset($_REQUEST["pk_renta"])
-    || !isset($_REQUEST["fk_arrendatario"])) {
+    || !isset($_REQUEST["user_id"])) {
     $mi0->abort(-1, NULL);
 }
 
 $pk_renta = $_REQUEST["pk_renta"];
-$fk_arrendatario = $_REQUEST["fk_arrendatario"];
+$user_id = $_REQUEST["user_id"];
+$user_id_sql = null;
 
 $mi0->begin();
+
+$mi0->query("
+    SELECT
+        tipo
+    FROM
+        usuario
+    WHERE pk_usuario = ?",
+    $user_id
+);
+if ($mi0->result->num_rows != 1) {
+    $mi0->end("rollback", -2, NULL);
+}
+
+$user_type = $mi0->result->fetch_all(MYSQLI_ASSOC)[0]["tipo"];
+$user_id_sql = ($user_type == "1") ? "renta.fk_arrendatario"
+    : "renta.fk_conductor";
 
 $mi0->query("
     SELECT
         fase
     FROM
         renta
-    WHERE pk_renta = ? AND fk_arrendatario = ?",
-    $pk_renta, $fk_arrendatario
+    WHERE pk_renta = ? AND $user_id_sql = ?",
+    $pk_renta, $user_id
 );
 if ($mi0->result->num_rows != 1) {
-    $mi0->end("rollback", -2, NULL);
+    $mi0->end("rollback", -3, NULL);
 }
 
 $fase = $mi0->result->fetch_all(MYSQLI_ASSOC)[0]["fase"];
 
-if ($fase == "0" || $fase == "2" || $fase == "4") {
-    $mi0->end("rollback", -3, NULL);
+if (($fase == "0" || $fase == "2" || $fase == "4") && $user_type == "1"
+    || ($fase == "1" || $fase == "3") && $user_type == "2"
+    ) {
+    $mi0->end("rollback", -4, NULL);
 }
 
 $fase = intval($fase) + 1;
@@ -41,6 +60,6 @@ $mi0->query("
     $fase, $pk_renta
 );
 if ($mi0->result !== TRUE) {
-    $mi0->end("rollback", -4, NULL);
+    $mi0->end("rollback", -5, NULL);
 }
 $mi0->end("commit", 0, NULL);
