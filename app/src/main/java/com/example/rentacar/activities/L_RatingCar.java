33a1d1eac2f4c7d891767dc0d259fd622fc995b4
rarity612 <hmoran@ucol.mx>
2021-06-08 22:53:21 -0500

    Agregada la section para calificar los autos rentados del arrendatario. (Falta terminar la funcionalidad)

---
+++ app/src/main/java/com/example/rentacar/activities/L_RatingCar.java
@@ -0,0 +1,16 @@
+package com.example.rentacar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rentacar.R;
+
+public class L_RatingCar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l_rating_car);
+    }
+}
---
+++ app/src/main/java/com/example/rentacar/fragments/L_RatingCar.java
@@ -0,0 +1,10 @@
+package com.example.rentacar.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class L_RatingCar extends Fragment {
+    public void setArguments(Bundle bundle) {
+    }
+}
