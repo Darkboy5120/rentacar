0d2811530e88ed537478f26eddc108bcff15a1c2
Roberto Contreras Montes <60668429+Estrater231199@users.noreply.github.com>
2021-06-16 00:30:59 -0500

    inclusión de Políticas y mas
    Se agrego:
    - Políticas de privacidad
    - Condiciones de uso
    - Misión
    - Valores
    - Origen

--- view/pages/main/info.php
+++ view/pages/main/info.php
@@ -30,11 +30,81 @@ $active_session = ($ci0->existSession("user_data")) ? "1" : "0";
                 <section class="content-wrapper">
                     <div class="content-index">
                         <a href="#privacy-policy">Politica de privacidad</a>
+                        <a href="#terms-of-use">Condiciones de uso</a>
+                        <a href="#mission">Misión</a>
+                        <a href="#values">Valores</a>
+                        <a href="#origin">Origen</a>
                     </div>
                     <div class="content">
                         <section class="content-item" id="privacy-policy">
                             <h2>Politica de privacidad</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores itaque enim sunt dignissimos beatae, ratione provident aliquid totam, cum, officia nesciunt debitis voluptatibus quos unde. Quaerat suscipit eius hic in.</p>
+                            <p>
+                                La presente Política de Privacidad establece los términos en que Chilaquiles Enterprise usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se emplea de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.
+
+                                Información que es recogida
+                                Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,  información de contacto como  su dirección de correo electrónico e información demográfica. Así mismo cuando sea necesario podrá ser requerida información específica para procesar algún pedido o realizar una entrega o facturación.
+
+                                Uso de la información recogida
+                                Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios. Es posible que sean enviados correos electrónicos periódicamente a través de nuestro sitio con ofertas especiales, nuevos productos y otra información publicitaria que consideremos relevante para usted o que pueda brindar algún beneficio, estos correos electrónicos serán enviados a la dirección que usted proporcione y podrán ser cancelados en cualquier momento.
+                                Chilaquiles Enterprise está altamente comprometido para cumplir con el compromiso de mantener su información segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos que no exista ningún acceso no autorizado.
+
+                                Cookies
+                                Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para tener información respecto al tráfico web, y también facilita las futuras visitas a una web recurrente. Otra función que tienen las cookies es que con ellas las web pueden reconocerse individualmente y por tanto brindarte el mejor servicio personalizado de su web.
+                                Nuestro sitio web emplea las cookies para poder identificar las páginas que son visitadas y su frecuencia. Esta información es empleada únicamente para análisis estadístico y después la información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, estas no dan acceso a información de su ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente . Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores aceptan cookies automáticamente pues sirve para tener un mejor servicio web. También usted puede cambiar la configuración de su ordenador para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.
+
+                                Enlaces a Terceros
+                                Este sitio web pudiera contener enlaces a otros sitios que pudieran ser de su interés. Una vez que usted dé clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos sitios están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar que usted está de acuerdo con estas.
+
+                                Control de su información personal
+                                En cualquier momento usted puede restringir la recopilación o el uso de la información personal que es proporcionada a nuestro sitio web. Cada vez que se le solicite rellenar un formulario, como el de alta de usuario, puede marcar o desmarcar la opción de recibir información por correo electrónico. En caso de que haya marcado la opción de recibir nuestro boletín o publicidad usted puede cancelarla en cualquier momento.
+                                Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con un orden judicial.
+                                Chilaquiles Enterprise se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.
+                            </p>
+                        <section class="content-item" id="terms-of-use">
+                            <h2>Condiciones de uso</h2>
+                            <p>
+                                Las presentes condiciones generales de uso de la página web, regulan los términos y condiciones de acceso y uso de https://chilaquilesenterprise.com/rentacar, propiedad de Chilaquiles Enterprise y con Código de Identificación Fiscal número D-51231199, en adelante, Chilaquiles Enterprise, que el usuario del Portal deberá de leer y aceptar para usar todos los servicios e información que se facilitan desde el portal. El mero acceso y/o utilización del portal, de todos o parte de sus contenidos y/o servicios significa la plena aceptación de las presentes condiciones generales de uso.
+
+                                Condiciones de uso
+                                Las presentes condiciones generales de uso del portal regulan el acceso y la utilización del portal, incluyendo los contenidos y los servicios puestos a disposición de los usuarios en y/o a través del portal, bien por el portal, bien por sus usuarios, bien por terceros. No obstante, el acceso y la utilización de ciertos contenidos y/o servicios puede encontrarse sometido a determinadas condiciones específicas.
+
+                                Modificaciones
+                                La empresa se reserva la facultad de modificar en cualquier momento las condiciones generales de uso del portal. En todo caso, se recomienda que consulte periódicamente los presentes términos de uso del portal, ya que pueden ser modificados.
+
+                                Obligaciones del Usuario
+                                El usuario deberá respetar en todo momento los términos y condiciones establecidos en las presentes condiciones generales de uso del portal. De forma expresa el usuario manifiesta que utilizará el portal de forma diligente y asumiendo cualquier responsabilidad que pudiera derivarse del incumplimiento de las normas.
+                                Así mismo, el usuario no podrá utilizar el portal para transmitir, almacenar, divulgar, promover o distribuir datos o contenidos que sean portadores de virus o cualquier otro código informático, archivos o programas diseñados para interrumpir, destruir o perjudicar el funcionamiento de cualquier programa o equipo informático o de telecomunicaciones.
+
+                                Responsabilidad del portal
+                                El usuario conoce y acepta que el portal no otorga ninguna garantía de cualquier naturaleza, ya sea expresa o implícita, sobre los datos, contenidos, información y servicios que se incorporan y ofrecen desde el Portal.
+                                Exceptuando los casos que la Ley imponga expresamente lo contrario, y exclusivamente con la medida y extensión en que lo imponga, el Portal no garantiza ni asume responsabilidad alguna respecto a los posibles daños y perjuicios causados por el uso y utilización de la información, datos y servicios del Portal.
+                                En todo caso, el Portal excluye cualquier responsabilidad por los daños y perjuicios que puedan deberse a la información y/o servicios prestados o suministrados por terceros diferentes de la Empresa. Toda responsabilidad será del tercero ya sea proveedor o colaborador.
+
+                                Propiedad intelectual e industrial
+                                Todos los contenidos, marcas, logos, dibujos, documentación, programas informáticos o cualquier otro elemento susceptible de protección por la legislación de propiedad intelectual o industrial, que sean accesibles en el portal corresponden exclusivamente a la empresa o a sus legítimos titulares y quedan expresamente reservados todos los derechos sobre los mismos. Queda expresamente prohibida la creación de enlaces de hipertexto https://chilaquilesenterprise.com/rentacar a cualquier elemento integrante de las páginas web del Portal sin la autorización de la empresa, siempre que no sean a una página web del Portal que no requiera identificación o autenticación para su acceso, o el mismo esté restringido.
+                                En cualquier caso, el portal se reserva todos los derechos sobre los contenidos, información datos y servicios que ostente sobre los mismos. El portal no concede ninguna licencia o autorización de uso al usuario sobre sus contenidos, datos o servicios, distinta de la que expresamente se detalle en las presentes condiciones generales de uso del portal.
+                                
+                                Legislación aplicable, jurisdicción competente y notificaciones
+                                Las presentes condiciones se rigen y se interpretan de acuerdo con las Leyes de España. Para cualquier reclamación serán competentes los juzgados y tribunales de Manzanillo, Colima. Todas las notificaciones, requerimientos, peticiones y otras comunicaciones que el Usuario desee efectuar a la Empresa titular del Portal deberán realizarse por escrito y se entenderá que han sido correctamente realizadas cuando hayan sido recibidas en la siguiente dirección rcontreras5@ucol.mx.
+                            </p>
+                        <section class="content-item" id="mission">
+                            <h2>Misión</h2>
+                            <p>
+                                “Ofrecemos un servicio de gestión para administradores de un negocio de renta de autos y una app para renta de autos y lo hacemos para proporcionar un servicio intermediario para la renta de autos disponible en todo momento.”
+                            </p>
+                        <section class="content-item" id="values">
+                            <h2>Valores</h2>
+                            <p>
+                                -Seguridad
+                                -Disponibilidad
+                                -Sinceridad
+                                -Integridad
+                            </p>
+                        <section class="content-item" id="origin">
+                            <h2>Origen</h2>
+                            <p>
+                                Esto empezó con la iniciativa de cumplir con el desarrollo del proyecto integrador, este proyecto debía cumplir con los estándares de la W3Schoos y después de 5 meses de trabajo, pruebas y documentación obtuvimos un producto completo y funcional.
+                            </p>
                         </section>
                     </div>
                 </section>
